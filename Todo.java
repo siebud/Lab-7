f74db250210c3841ce2e59e446d5008c91a924c1
Sierra Budinoff <sierra.j.budinoff-1@ou.edu>
2019-03-10 23:41:35 -0500

    Todo

--- Todo.java
+++ Todo.java
@@ -14,6 +14,11 @@ Tasks:
 7. StackHospital lists and treating methods
 8. QueueHospital string return methods
 9. QueueHospital lists and treating methods
+10. PriorityQueueHospital string return methods
+11. PriorityQueueHospital lists and treating methods
+12. PersonTest writing
+13. AnimalTest Writing
+14. HospitalTests
 
 Estimated Time (minutes):
 1. 15
@@ -25,15 +30,25 @@ Estimated Time (minutes):
 7. 40
 8. 25
 9. 35
+10. 10
+11. 30
+12. 45
+13. 30
+14. 60
 
 Actual Time (minutes):
 1. 15
 2. 10
 3. 10
 4. 10
-5. 30 ++
+5. 40
 6. 10
 7. 10
-8. 
-9. 
+8. 10
+9. 10
+10. 10
+11. 50
+12. 30
+13. 20
+14. 90
 */}
